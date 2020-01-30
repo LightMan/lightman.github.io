4634c5a4f044b3388fd8edec6c3174d528958c95
David Jorge <david.jorge@appconcept.es>
2020-01-30 14:21:06 +0100

    add react initial commit

---
+++ precache-manifest.f89d51ee2236a69fcfbbbeb45b7f0646.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b50bc8f4faed4c9d5453f5b4fa896c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9616f3ee34a108052a1c",
+    "url": "/static/css/main.549b3011.chunk.css"
+  },
+  {
+    "revision": "451381563ca3134cf430",
+    "url": "/static/js/2.8315f2cf.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.8315f2cf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9616f3ee34a108052a1c",
+    "url": "/static/js/main.34beb830.chunk.js"
+  },
+  {
+    "revision": "1b101eb11157ca6028d2",
+    "url": "/static/js/runtime-main.97e782ed.js"
+  },
+  {
+    "revision": "093627dd733e697e3924802f9b4c9f85",
+    "url": "/static/media/keepcalm-medium-webfont.093627dd.woff2"
+  }
+]);
