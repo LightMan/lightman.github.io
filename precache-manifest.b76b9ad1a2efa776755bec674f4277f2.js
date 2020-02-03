d6e7ebb37f2997e0c79d1afe4ad69e0a0e6a8c68
David Jorge <david.jorge@appconcept.es>
2020-02-03 09:31:19 +0100

    Updates

---
+++ precache-manifest.b76b9ad1a2efa776755bec674f4277f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9481a6fcf0b34c2623e49d8196098ab",
+    "url": "/appconcept/index.html"
+  },
+  {
+    "revision": "67541a3578a2614f170a",
+    "url": "/appconcept/static/css/main.1893697e.chunk.css"
+  },
+  {
+    "revision": "f1e825acd05a351e620a",
+    "url": "/appconcept/static/js/2.5aa13052.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/appconcept/static/js/2.5aa13052.chunk.js.LICENSE"
+  },
+  {
+    "revision": "67541a3578a2614f170a",
+    "url": "/appconcept/static/js/main.f6dbf3a0.chunk.js"
+  },
+  {
+    "revision": "bed9cf768880e6fa9f28",
+    "url": "/appconcept/static/js/runtime-main.6e813977.js"
+  },
+  {
+    "revision": "093627dd733e697e3924802f9b4c9f85",
+    "url": "/appconcept/static/media/keepcalm-medium-webfont.093627dd.woff2"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.582ee25406731fa45cbd56d8d09efdb3.js"
+  "/appconcept/precache-manifest.b76b9ad1a2efa776755bec674f4277f2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/appconcept/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
